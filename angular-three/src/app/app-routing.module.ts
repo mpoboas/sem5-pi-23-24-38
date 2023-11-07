71227a643a531fe93df36f221dd8a8e6946c0984
Miguel Póvoas <1201716@isep.ipp.pt>
2023-11-07 18:55:42 +0000

    Added cube template
    Basic angular example

--- angular-three/src/app/app-routing.module.ts
+++ angular-three/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CubeComponent } from './cube/cube.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/cube', pathMatch: 'full'},
+  {path: 'cube', component: CubeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
